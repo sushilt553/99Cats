bcf2d0f43b9ab002bf04d7a4e314eab0e9552092
Sushil <sushilt553@gmail.com>
2020-06-25 12:34:17 -0400

    Cat model and instance complete

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,15 @@
+require 'action_view'
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
+    CAT_COLORS =  %w(White Black Brown Blue Yellow Green Red Pink)
+
+    validates :birth_date, :color, :name, :sex, :description, presence: true
+    validates :sex, inclusion: { in: %w(M F) }
+    validates :color, inclusion: CAT_COLORS
+
+    def age
+        time_ago_in_words(self.birth_date)
+    end
 end
