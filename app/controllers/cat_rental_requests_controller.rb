0358b52c3da1f94feb27f3d23c12e829ea57b2e3
Sushil <sushilt553@gmail.com>
2020-06-25 16:25:08 -0400

    Cat rental requests controller complete

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -6,10 +6,18 @@ class CatRentalRequestsController < ApplicationController
     end
 
     def create
-        
+        @cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
+
+        if @cat_rental_request.save
+            redirect_to cat_url(@cat_rental_request.cat)
+        else
+            flash.now[:errors] = @cat_rental_request.errors.full_messages
+            render :new
+        end
     end
 
     private
     def cat_rental_request_params
+        params.require(:cat_rental_request).permit(:start_date, :end_date, :cat_id)
     end
 end
