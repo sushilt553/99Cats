58b0571a17fd2b7cc0fa21953d97a41acc51819b
Sushil <sushilt553@gmail.com>
2020-06-25 15:05:42 -0400

    Edit form complete

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -18,4 +18,35 @@ class CatsController < ApplicationController
     def new
         render :new
     end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cats_url
+        else
+            render json: @cat.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find_by(id: params[:id])
+
+        if @cat.update_attributes(cat_params)
+            redirect_to cat_url(@cat.id)
+        else
+            render json: @cat.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :color, :sex, :birth_date, :description)
+    end
 end
