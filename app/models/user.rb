a63bca614c54f5b5216e52502f14e80f450c115a
Sushil <sushilt553@gmail.com>
2020-06-26 13:21:57 -0400

    User authentication test in pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
 
@@ -20,13 +24,13 @@ class User < ApplicationRecord
     end
 
     def is_password?(password) 
-        bcrypted_password = BCrypt.new(self.password_digest)
+        bcrypted_password = BCrypt::Password.new(self.password_digest)
         bcrypted_password.is_password?(password)
     end
     
     def password=(password) 
         @password = password
-        self.password_digest = BCrypt.create(password)
+        self.password_digest = BCrypt::Password.create(password)
         self.save
     end
 
