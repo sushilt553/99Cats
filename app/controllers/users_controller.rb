cdec9b4f8dd6ce57ecc6324adde696a262d6e427
Sushil <sushilt553@gmail.com>
2020-06-26 14:27:05 -0400

    User login complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            redirect_to cats_url
+            log_in(@user)
         else
             flash.now[:errors] = @user.errors.full_messages
             render :new
