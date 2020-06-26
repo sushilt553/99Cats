7f397e778e3cc982787c537409f7efff979f84e7
Sushil <sushilt553@gmail.com>
2020-06-26 13:48:29 -0400

    Session Controller done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def log_in(user)
+        session[:session_token] = user.reset_session_token!
+        redirect_to cats_url
+    end
+
+    def log_out
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        redirect_to new_session_url
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            log_in(@user)
+        else
+            flash.now[:errors] = ["Username/Password didn't match"]
+            render :new
+        end
     end
 
     def destroy
+        log_out
     end
 end
